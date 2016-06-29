893fdfdf7e986505e69995d0860eb52201504962
Muzzammil <muzzammil.ehti@gmail.com>
2016-06-29 16:42:41 +0530

    New class added

---
+++ src/main/java/testleaf/Newclassadded.java
@@ -0,0 +1,16 @@
+package testleaf;
+
+import org.testng.annotations.Test;
+
+
+public class Newclassadded {
+
+	@Test
+	public void newClass()
+	{
+		System.out.println("New class is added");
+		
+	}
+	
+	
+}
